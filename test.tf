0d2541ce618fe6fe0d95ac0c0f471d7f159ade78
jst-r <jstr271@gmail.com>
2024-09-23 10:07:57 +0200

    get rid of cloudfront

--- test.tf
+++ test.tf
@@ -62,75 +62,18 @@ resource "aws_iam_role_policy" "lambda_policy" {
 }
 
 #########################
-# Api gateway
-
-resource "aws_api_gateway_rest_api" "api" {
-  name        = "my_api"
-  description = "API Gateway for Lambda"
-}
-
-resource "aws_api_gateway_resource" "api_resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "lambda"
-}
-
-resource "aws_api_gateway_method" "api_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.api_resource.id
-  http_method             = aws_api_gateway_method.api_method.http_method
-  type                    = "AWS_PROXY"
-  integration_http_method = "POST"
-  uri                     = aws_lambda_function.example_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.example_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-resource "aws_api_gateway_deployment" "api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
-  depends_on = [
-    aws_api_gateway_integration.lambda_integration,
-  ]
-}
-
-output "api_endpoint" {
-  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.api_deployment.stage_name}/lambda"
-}
-
-#########################
 # S3
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "static_website" {
   bucket = "jstre-iu-cloud-programming-bucket"
-}
 
-resource "aws_s3_bucket_acl" "static_website_acl" {
-  bucket = aws_s3_bucket.static_website.id
-  acl    = "private"
-}
-
-# Enable versioning for the S3 bucket
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.static_website.id
-  versioning_configuration {
-    status = "Enabled"
+  website {
+    index_document = "index.html"
   }
 }
+
+resource "aws_s3_bucket_acl" ""
 
 # Upload an index.html file to the S3 bucket
 resource "aws_s3_object" "index" {
@@ -138,56 +81,75 @@ resource "aws_s3_object" "index" {
   key    = "index.html"
   source = "static/index.html"
   etag   = filemd5("static/index.html")
+  acl    = "public-read"
 }
 
-locals {
-  s3_origin_id = "static_s3_origin_id"
+#########################
+# API Gateway
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "example-api"
+  description = "Example API to serve S3 and Lambda"
 }
 
-
-# Create a CloudFront distribution
-resource "aws_cloudfront_distribution" "static_website_distribution" {
-
-  origin {
-    domain_name              = aws_s3_bucket.static_website.bucket_regional_domain_name
-    origin_id                = local.s3_origin_id
-    origin_access_control_id = aws_cloudfront_origin_access_control.main.id
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+# Root resource serving static S3 website
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = ""
 }
 
-output "cloudfront_distribution_url" {
-  value = "https://${aws_cloudfront_distribution.static_website_distribution.domain_name}"
+resource "aws_api_gateway_method" "root_method" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.root_method.http_method
+  type        = "HTTP"
+  uri         = "http://${aws_s3_bucket.static_website.bucket}.s3-website.${var.region}.amazonaws.com"
+  integration_http_method = "GET"
+}
+
+# /api/increment_counter resource
+resource "aws_api_gateway_resource" "api_increment_counter" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "api/increment_counter"
+}
+
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.api_increment_counter.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.api_increment_counter.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.example_lambda.invoke_arn
+  integration_http_method = "POST"
+}
+
+# Grant API Gateway permissions to invoke the Lambda function
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/POST/api/increment_counter"
+}
+
+#########################
+# Outputs
+
+output "api_endpoint" {
+  value = aws_api_gateway_deployment.example_api.invoke_url
 }
